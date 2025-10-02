fbb5611eebd444d543414fdb09495ec0107aef8c
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-10-02 23:30:59 +0300

    Add project files.

---
+++ Blogy.Entity/Entities/BlogTag.cs
@@ -0,0 +1,18 @@
+﻿using Blogy.Entity.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.Entity.Entities
+{
+    public class BlogTag: BaseEntity
+    {
+        public int TagId { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+        public Tag Tag { get; set; }
+       
+    }
+}
